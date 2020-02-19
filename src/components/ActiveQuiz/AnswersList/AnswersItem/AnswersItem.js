9c295e030a1afb579862d5d3d716278908139a04
Aleksey Gunchin <a.s.gunchin@gmail.com>
2020-02-19 16:45:28 +0400

    modified response status display

--- src/components/ActiveQuiz/AnswersList/AnswersItem/AnswersItem.js
+++ src/components/ActiveQuiz/AnswersList/AnswersItem/AnswersItem.js
@@ -3,9 +3,15 @@ import classes from './AnswersItem.css'
 // import AnswersList from '../AnswersList'
 
 const AnswersItem = props => {
+  const cls = [classes.answersItem]
+
+  if (props.state) {
+    cls.push(classes[props.state])
+  }
+
   return (
     <li
-      className={classes.answersItem}
+      className={cls.join(' ')}
       onClick={() => props.onAnswerClick(props.answer.id)}
     >
       {props.answer.text}
--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -5,6 +5,7 @@ import ActiveQuiz from '../../components/ActiveQuiz/ActiveQuiz'
 class Quiz extends Component {
   state = {
     activeQuestion: 0,
+    answerState: null,
     quiz: [
       {
         question: 'Какого цвета небо?',
@@ -19,8 +20,8 @@ class Quiz extends Component {
       },
       {
         question: 'В каком году основали Санкт-Петербург?',
-        rightAnswerId: 2,
-        id: 3,
+        rightAnswerId: 3,
+        id: 2,
         answers: [
           {text: '1700', id: 1},
           {text: '1002', id: 2},
@@ -32,10 +33,31 @@ class Quiz extends Component {
   }
 
   onAnswerClickHandler = answerId => {
-    console.log('Answer Id: ', answerId)
-    this.setState({
-      activeQuestion: this.state.activeQuestion + 1
-    })
+    const question = this.state.quiz[this.state.activeQuestion]
+    if (question.rightAnswerId === answerId) {
+      this.setState({
+        answerState: {[answerId]: 'success'}
+      })
+      const timeout = window.setTimeout(() => {
+        if(this.isQuizFinised()) {
+          console.log('Finished')
+        } else {
+          this.setState({
+            activeQuestion: this.state.activeQuestion + 1,
+            answerState: null
+          })
+        }
+        window.clearTimeout(timeout)
+      }, 1000)
+    } else {
+      this.setState({
+        answerState: {[answerId]: 'error'}
+      })
+    }
+  }
+
+  isQuizFinised() {
+    return this.state.activeQuestion + 1 === this.state.quiz.length
   }
 
   render() {
@@ -49,6 +71,7 @@ class Quiz extends Component {
             onAnswerClick={this.onAnswerClickHandler}
             quizLength={this.state.quiz.length}
             answerNumber={this.state.activeQuestion + 1}
+            state={this.state.answerState}
           />
         </div>
       </div>
