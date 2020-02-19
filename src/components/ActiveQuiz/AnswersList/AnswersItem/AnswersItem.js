ca7cd0b8c8b6417138ceb4afc352e497c364dbff
Aleksey Gunchin <a.s.gunchin@gmail.com>
2020-02-19 16:13:37 +0400

    add click processing

--- src/components/ActiveQuiz/AnswersList/AnswersItem/AnswersItem.js
+++ src/components/ActiveQuiz/AnswersList/AnswersItem/AnswersItem.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import classes from './AnswersItem.css'
-import AnswersList from '../AnswersList'
+// import AnswersList from '../AnswersList'
 
 const AnswersItem = props => {
   return (
-    <li className={classes.answersItem}>
+    <li
+      className={classes.answersItem}
+      onClick={() => props.onAnswerClick(props.answer.id)}
+    >
       {props.answer.text}
     </li>
   )
