d1ca901f6ae485bdb22157954c20d931628409dc
Aleksey Gunchin <a.s.gunchin@gmail.com>
2020-02-19 15:21:27 +0400

    modified component of the active question

--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import classes from './Quiz.css'
+import ActiveQuiz from '../../components/ActiveQuiz/ActiveQuiz'
 
 class Quiz extends Component {
   state = {
@@ -8,7 +9,10 @@ class Quiz extends Component {
   render() {
     return(
       <div className={classes.quiz}>
-        <h1>Quiz</h1>
+        <div className={classes.quizWrapper}>
+          <h1>Quiz</h1>
+          <ActiveQuiz />
+        </div>
       </div>
     )
   }
