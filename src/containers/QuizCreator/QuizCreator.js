5ccb023e6082aab311e0f99c085044fcbe364ec7
Aleksey Gunchin <a.s.gunchin@gmail.com>
2020-03-02 18:52:20 +0400

    added pages

---
+++ src/containers/QuizCreator/QuizCreator.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+export default class QuizCreator extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Quiz Creator</h2>
+      </div>
+    )
+  }
+}
---
+++ src/containers/QuizList/QuizList.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+export default class QuizList extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Quiz List</h2>
+      </div>
+    )
+  }
+}
