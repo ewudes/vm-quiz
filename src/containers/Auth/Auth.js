614e7f630c138e9612ab8ec91d173db4d1204fef
Aleksey Gunchin <a.s.gunchin@gmail.com>
2020-03-03 16:53:05 +0400

    added authorization page

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,10 +1,46 @@
 import React, {Component} from 'react'
+import classes from './Auth.css'
+import Button from '../../components/UI/Button/Button'
 
 export default class Auth extends Component {
+
+  loginHandler = () => {
+
+  }
+  
+  registerHandler = () => {
+    
+  }
+
+  submitHandler = event => {
+    event.preventDefault()
+  }
+
   render() {
     return (
-      <div>
-        <h2>Auth</h2>
+      <div className={classes.auth}>
+        <div>
+          <h2>Авторизация</h2>
+          <form
+            onSabmit={this.submitHandler}
+            className={classes.authForm}
+          >
+            <input type="text" />
+            <input type="text" />
+            <Button
+              type="success"
+              onClick={this.loginHandler}
+            >
+              Войти
+            </Button>
+            <Button
+              type="primary"
+              onClick={this.registerHandler}
+            >
+              Зарегистрироваться
+            </Button>
+          </form>
+        </div>
       </div>
     )
   }
