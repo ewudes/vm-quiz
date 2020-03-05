84a26ce85ffa0d665b940d5c851bbfda37d478ff
Aleksey Gunchin <a.s.gunchin@gmail.com>
2020-03-05 19:15:05 +0400

    modified form validation

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -7,6 +7,7 @@ import is from 'is_js'
 export default class Auth extends Component {
 
   state = {
+    isFormValid: false,
     formControls: {
       email: {
         value: '',
@@ -74,8 +75,14 @@ export default class Auth extends Component {
 
     formControls[controlName] = control
 
+    let isFormValid = true
+
+    Object.keys(formControls).forEach(name => {
+      isFormValid = formControls[name].valid && isFormValid
+    })
+
     this.setState({
-      formControls
+      formControls, isFormValid
     })
   }
  
@@ -111,12 +118,14 @@ export default class Auth extends Component {
             <Button
               type="success"
               onClick={this.loginHandler}
+              disabled={!this.state.isFormValid}
             >
               Войти
             </Button>
             <Button
               type="primary"
               onClick={this.registerHandler}
+              disabled={!this.state.isFormValid}
             >
               Зарегистрироваться
             </Button>
