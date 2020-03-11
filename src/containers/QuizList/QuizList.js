4a79b939a33ffb9173dc020475203015ee3258b9
Aleksey Gunchin <a.s.gunchin@gmail.com>
2020-03-11 15:27:56 +0400

    added axios

--- src/containers/QuizList/QuizList.js
+++ src/containers/QuizList/QuizList.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import classes from './QuizList.css'
 import { NavLink } from 'react-router-dom'
+import axios from 'axios'
 
 export default class QuizList extends Component {
   
@@ -13,6 +14,12 @@ export default class QuizList extends Component {
           </NavLink>
         </li>
       )
+    })
+  }
+
+  componentDidMount() {
+    axios.get('https://react-quiz-b4542.firebaseio.com/quiz.json').then(response => {
+      console.log(response)
     })
   }
 
